82ac8f0838108b5383c9d3025a9408e77e93f33e
carcasonne <villumsonne@protonmail.com>
2024-12-13 14:59:54 +0100

    a new run

--- src/main.py
+++ src/main.py
@@ -2,7 +2,7 @@ from torchvision import transforms
 
 from Application.ClonedAudioDetector import CNNClassifier
 from Application.DataProcessor import DataProcessor
-from Application.DataSource import DataType, LocalDataSource
+from Application.DataSource import LocalDataSource
 from config import *
 from config import AudioClassifierConfig
 from Core.ResultsDashboardGenerator import ColorScheme, TrainingReporter
@@ -28,11 +28,11 @@ config = AudioClassifierConfig(
     img_height=625,
     img_width=469,
     channels=3,
-    duration=2,
+    duration=5,
     # misc
     notes="The data was trained on the Cloned Audio CNN Classifier defined in the paper: 'Fighting AI with AI: Fake Speech Detection using Deep Learning' by Malik & Changalvala.",
-    data_path="../Ar-DAD_Arabic_Diversified",
-    #data_path="../audio_files_fake_from_paper",
+    data_path="../audio_files_fake_from_paper",
+    # data_path="../audio_files_fake_from_paper",
     output_path="../output",
     run_cuda=True,
 )
@@ -45,7 +45,7 @@ transform_normalization = transforms.Compose(
 data_source = LocalDataSource(
     config.data_path, config.sample_rate, config.duration, transform_normalization
 )
-ASV_data = data_source.get_ARAB_dataset(380)
+data = data_source.get_k_fold_dataset()
 
 # Scuffed but cba fixing
 training_data = None
@@ -62,12 +62,12 @@ reporter = TrainingReporter(
     training_data=training_data,
     validation_data=validation_data,
     testing_data=testing_data,
-    color_scheme=ColorScheme.FRIEREN,
+    color_scheme=ColorScheme.LIGHT,
     base_dir="../training_runs",
 )
 
 results, complete_time, models = data_processor.process_k_fold(
-    CNNClassifier, ASV_data, config.k_folds, config.epochs, config.batch_size, config.run_cuda
+    CNNClassifier, data, config.k_folds, config.epochs, config.batch_size, config.run_cuda
 )
 
 data_processor.save_kfold_models(models)
